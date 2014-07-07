82c4ca644abc303e57b276b29de866b22b01d19a
Carl-Erik Kopseng <carlerik@gmail.com>
2014-07-07 10:21:46 +0200

    Added Google Analytics tracking of login

--- client/views/login.js
+++ client/views/login.js
@@ -1,23 +1,58 @@
 Template.login.events({
 
 	'click #loginPersona' : function () {
+		ga('send', {
+			'hitType': 'event',          // Required.
+			'eventCategory': 'login',   // Required.
+			'eventAction': 'click',      // Required.
+			'eventLabel': '#loginPersona'
+		});
+
 		Meteor.loginWithPersona(function (err) {
 			if (err) {
 				console.error('error', err);
+				ga('send', {
+					'hitType': 'event',          // Required.
+					'eventCategory': 'login',   // Required.
+					'eventAction': 'error',      // Required.
+					'eventLabel': '#loginPersona'
+				});
 			}
 			else {
-				console.log('logged in persona', arguments);
+				ga('send', {
+					'hitType': 'event',          // Required.
+					'eventCategory': 'login',   // Required.
+					'eventAction': 'success',      // Required.
+					'eventLabel': '#loginPersona'
+				});
 			}
 		});
 	},
 
 	'click #loginFacebook' : function () {
+		ga('send', {
+			'hitType': 'event',          // Required.
+			'eventCategory': 'login',   // Required.
+			'eventAction': 'click',      // Required.
+			'eventLabel': '#loginFacebook'
+		});
 		Meteor.loginWithFacebook(function (err) {
 			if (err) {
 				console.error('error facebook', arguments);
+				ga('send', {
+					'hitType': 'event',          // Required.
+					'eventCategory': 'login',   // Required.
+					'eventAction': 'error',      // Required.
+					'eventLabel': '#loginFacebook'
+				});
 			}
 			else {
-				console.log('logged in facebook', arguments);
+				ga('send', {
+					'hitType': 'event',          // Required.
+					'eventCategory': 'login',   // Required.
+					'eventAction': 'success',      // Required.
+					'eventLabel': '#loginFacebook'
+				});
 			}
 		});
 	}
